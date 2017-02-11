b839f670a820557d04afb04dadbb36f1b031e2eb
Sebastian Herrera <herrerasebastian55@gmail.com>
2017-02-10 21:54:08 -0500

    contact us tres

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
-
+ 
+  match 'contactUS', to: 'pages#contactUS', via: :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
